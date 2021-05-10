c0a8ff8e65958a1bdd99cb3cb67da0024405a5a7
dorothytest <tangmingxia037052@163.com>
2021-05-10 16:27:33 +0800

    add AppTest2

---
+++ Java/src/test/java/com/github/vitalliuss/helloci/AppTest2.java
@@ -0,0 +1,12 @@
+package com.github.vitalliuss.helloci;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class AppTest2 {
+    @Test
+    public void testShouldBePassed() {
+        assertTrue(true);
+    }
+}
